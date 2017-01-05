1b2e6026fc8acbd27e9fa44c2c6c9f6545b3d30b
Jonathan Chapman <chapmajs@gmail.com>
2017-01-04 23:04:41 -0500

    Add SendGrid config for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :user_name            => Rails.application.secrets[:email]['username'],
+    :password             => Rails.application.secrets[:email]['password'],
+    :address              => 'smtp.sendgrid.net',
+    :domain               => 'smtp.sendgrid.net',
+    :port                 => Rails.application.secrets[:email]['port'],
+    :enable_starttls_auto => true
+  }
 end
