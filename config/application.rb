2c29c39f1efb2c4794c05c5bec1c00c5835c7cc7
Jonathan Chapman <chapmajs@gmail.com>
2017-01-04 23:00:24 -0500

    Upgrading to Rails 5, file cleanup

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+#require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+#require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,20 +21,17 @@ module RailsServices
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     # Autoload services
     config.autoload_paths += Dir["#{Rails.root}/app/services"]
+
+    # Enable flash messages, required in Rails 5+
+    config.middleware.use ActionDispatch::Flash
+
+    # Disable cookies, we don't use them here
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+
+    config.active_record.primary_key = :uuid
   end
 end
