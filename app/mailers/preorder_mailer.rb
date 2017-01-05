5e8ce00d8c3d1342602c64cc47ea7f6c21227d6a
Jonathan Chapman <chapmajs@gmail.com>
2017-01-05 12:18:44 -0500

    Using path helper instead of hardcoded string in confirmation email

--- app/mailers/preorder_mailer.rb
+++ app/mailers/preorder_mailer.rb
@@ -1,7 +1,9 @@
 class PreorderMailer < ApplicationMailer
+  include Rails.application.routes.url_helpers
 
   def confirmation(preorder)
     @preorder = preorder
+    @confirm_path = confirm_preorders_path(:token => @preorder.confirmation_token)
 
     mail(
       :to => @preorder.email,
