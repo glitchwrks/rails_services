cff27ad55d4807fefe2010dc0959df01e6c2c1f7
Jonathan Chapman <chapmajs@gmail.com>
2017-01-05 11:21:56 -0500

    Adding scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,27 @@
+module RouteScope
+  def self.root
+    Rails.application.config.root_directory
+  rescue NameError
+    '/'
+  end
+end
+
 Rails.application.routes.draw do
+  scope RouteScope.root do
 
-  root :to => proc { [404, {}, ['Not found.']] }
+    root :to => proc { [404, {}, ['Not found.']] }
 
-  resources :projects, :only => [] do
-    member do
-      get :disabled
+    resources :projects, :only => [] do
+      member do
+        get :disabled
+      end
+    end
+
+    resources :preorders, :only => [:new, :create] do
+      collection do
+        get :success
+        get :confirm
+      end
     end
   end
-
-  resources :preorders, :only => [:new, :create] do
-    collection do
-      get :success
-      get :confirm
-    end
-  end
-
 end
