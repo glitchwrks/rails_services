091a6086317631b4625c653b5bb452db520cc333
Jonathan Chapman <chapmajs@gmail.com>
2018-01-17 16:46:34 -0500

    Adding boolean field option definitions to TestfdcResult

--- app/models/testfdc_result.rb
+++ app/models/testfdc_result.rb
@@ -1,6 +1,9 @@
 class TestfdcResult < ApplicationRecord
   belongs_to :note, :class_name => 'TestfdcNote'
 
+  RESULT_OPTIONS = [['Pass', 1], ['Fail', 0], ['Not Tested', '']]
+  NOT_TESTED = ''
+
   def approve!
   	self.update_attributes!(:approved => true)
   end
