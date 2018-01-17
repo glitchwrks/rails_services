fe38d31e56a1969c21bac9034ffec222e2be7704
Jonathan Chapman <chapmajs@gmail.com>
2018-01-17 17:07:46 -0500

    Adding spec, factory defs, disallowing blank reports

--- app/models/testfdc_result.rb
+++ app/models/testfdc_result.rb
@@ -1,10 +1,18 @@
 class TestfdcResult < ApplicationRecord
   belongs_to :note, :class_name => 'TestfdcNote'
 
+  validate :at_least_one_field_populated
+
   RESULT_OPTIONS = [['Pass', 1], ['Fail', 0], ['Not Tested', '']]
   NOT_TESTED = ''
 
   def approve!
-  	self.update_attributes!(:approved => true)
+    self.update_attributes!(:approved => true)
+  end
+
+  private
+
+  def at_least_one_field_populated
+    errors[:base] << 'You submitted a blank report; you must fill in something!' unless manufacturer.present? || model.present? || fdc_chip.present?
   end
 end
---
+++ spec/features/create_testfdc_result_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'Submit a TestFDC result report' do
+
+  before(:each) do
+    visit new_testfdc_result_path
+  end
+
+  scenario 'User submits a blank form' do
+    click_button 'Submit Your TestFDC Results'
+    expect(page).to have_text 'You submitted a blank report; you must fill in something!'
+  end
+
+  describe 'when the user fills in something' do
+    before(:each) do 
+      fill_in :manufacturer, :with => 'ACME Corp'
+      click_button 'Submit Your TestFDC Results'
+    end
+
+    it { expect(page).to have_text 'Thanks for your TestFDC results submission! It will be reviewed shortly.' }
+
+    it 'should not display the unapproved result in the main registry page' do
+      visit testfdc_results_path
+      expect(page).not_to have_text 'ACME Corp'
+    end
+  end
+
+end
