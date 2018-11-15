0664c2b7c97c958f3ae44c161b9a3f09d88b76cc
Jonathan Chapman <chapmajs@gmail.com>
2018-11-15 15:33:55 -0500

    Fixing Capistrano version in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.7.1"
+lock "3.11.0"
 
 set :application, 'rails_services'
 set :repo_url, 'git@github.com:glitchwrks/rails_services.git'
