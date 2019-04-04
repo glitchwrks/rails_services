03333defba7e0e91908d75389075db81b58d6b2c
Jonathan Chapman <chapmajs@gmail.com>
2019-04-03 23:11:58 -0400

    Setting env variables for cap assets task

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,9 @@ set :rvm_ruby_version, '2.6.2@rails_services'
 # Defaults to web only
 set :assets_roles, [:web, :app]
 
+# RAILS_GROUPS env value for the assets:precompile task. Default to nil.
+set :rails_assets_groups, :assets
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
