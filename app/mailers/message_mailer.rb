c3b2a0c0f208fbc52c336ad20efb69620aaa11e1
Jonathan Chapman <chapmajs@gmail.com>
2020-05-27 23:18:12 -0400

    Updating from: field for contact form mails

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -4,7 +4,6 @@ class MessageMailer < ApplicationMailer
     @message = message
 
     mail(
-      :from => @message.email,
       :reply_to => @message.email,
       :to => ApplicationSetting.find_by_name('contact_email'),
       :subject => "#{@message.sanitized_subject} (from contact form)",
