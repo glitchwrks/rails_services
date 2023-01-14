5d65be17c07cc6072d55deeed83ed11f8c1d2296
Jonathan Chapman <chapmajs@gmail.com>
2023-01-14 10:27:03 -0500

    Improving code coverage for Projects/Preorders

--- spec/features/create_preorder_spec.rb
+++ spec/features/create_preorder_spec.rb
@@ -5,49 +5,60 @@ feature 'Create a preorder for a project' do
   context 'with an enabled project' do
     let!(:project) { FactoryBot.create(:enabled_project) }
 
-    before(:each) do
-      visit new_preorder_path(:project => project.name)
+    scenario 'User goes to project page to find a preorderable project' do
+      visit projects_path
+      expect(page).to have_text 'This is a list of all active projects'
+      expect(page).to have_text(project.printable_name)
+      click_on 'Sign Up for Preorder'
+      expect(page).to have_text 'Please fill out the following information to preorder'
     end
 
-    scenario 'User submits a blank form' do
-      click_button 'Create Preorder'
-      expect(page).to have_text('You must specify at least one quantity')
-      expect(page).to have_text("can't be blank")
-    end
+    context 'from the Create Preorder page' do
 
-    scenario 'User submits a duplicate email for the same project' do
-      FactoryBot.create(:populated_preorder, :project => project, :email => 'dupe@glitchwrks.com')
+      before(:each) do
+        visit new_preorder_path(:project => project.name)
+      end
 
-      fill_in :email, :with => 'dupe@glitchwrks.com'
-      click_button 'Create Preorder'
-      expect(page).to have_text('already registered for this project')
-    end
+      scenario 'User submits a blank form' do
+        click_button 'Create Preorder'
+        expect(page).to have_text('You must specify at least one quantity')
+        expect(page).to have_text("can't be blank")
+      end
 
-    scenario 'User fails the CAPTCHA' do
-      expect_any_instance_of(RecaptchaVerificationService).to receive(:execute).and_return(false)
+      scenario 'User submits a duplicate email for the same project' do
+        FactoryBot.create(:populated_preorder, :project => project, :email => 'dupe@glitchwrks.com')
 
-      fill_in :name, :with => 'The Tester'
-      fill_in :email, :with => 'test@glitchwrks.com'
-      fill_in 'bare boards', :with => '1'
-      click_button 'Create Preorder'
-      expect(page).to have_text('CAPTCHA failed, please try again')
-    end
+        fill_in :email, :with => 'dupe@glitchwrks.com'
+        click_button 'Create Preorder'
+        expect(page).to have_text('already registered for this project')
+      end
 
-    scenario 'User creates a preorder for an enabled project' do
-      expect_any_instance_of(RecaptchaVerificationService).to receive(:execute).and_return(true)
+      scenario 'User fails the CAPTCHA' do
+        expect_any_instance_of(RecaptchaVerificationService).to receive(:execute).and_return(false)
 
-      expect(page).to have_text("one or more #{project.printable_name}")
+        fill_in :name, :with => 'The Tester'
+        fill_in :email, :with => 'test@glitchwrks.com'
+        fill_in 'bare boards', :with => '1'
+        click_button 'Create Preorder'
+        expect(page).to have_text('CAPTCHA failed, please try again')
+      end
 
-      fill_in :name, :with => 'The Tester'
-      fill_in :email, :with => 'test@glitchwrks.com'
-      fill_in 'bare boards', :with => '1'
-      fill_in :kits, :with => '2'
-      fill_in :assembled, :with => '3'
-      click_button 'Create Preorder'
+      scenario 'User creates a preorder for an enabled project' do
+        expect_any_instance_of(RecaptchaVerificationService).to receive(:execute).and_return(true)
 
-      expect(page).to have_text('Preorder Successfully Submitted')
-      expect(Preorder.find_by(:name => 'The Tester', :project_id => project.id)).to be_present
-      expect(Preorder.find_by(:name => 'The Tester', :project_id => project.id).confirmation_token).to be_present
+        expect(page).to have_text("one or more #{project.printable_name}")
+
+        fill_in :name, :with => 'The Tester'
+        fill_in :email, :with => 'test@glitchwrks.com'
+        fill_in 'bare boards', :with => '1'
+        fill_in :kits, :with => '2'
+        fill_in :assembled, :with => '3'
+        click_button 'Create Preorder'
+
+        expect(page).to have_text('Preorder Successfully Submitted')
+        expect(Preorder.find_by(:name => 'The Tester', :project_id => project.id)).to be_present
+        expect(Preorder.find_by(:name => 'The Tester', :project_id => project.id).confirmation_token).to be_present
+      end
     end
   end
 
